f0bdc228f7e6028765d00f3ff671474520dba0de
djorgeext <djorge.@fi.uba.ar>
2025-03-18 16:46:43 -0300

    last

--- js/day4.js
+++ js/day4.js
@@ -2,5 +2,24 @@ const fs = require('fs');
 const data = fs.readFileSync('input4.txt', 'utf8');
 const log = console.log;
 const lines = data.trim().split('\n');
+const forwardMatch = "XMAS";
+const backwardMatch = "SAMX";
 
-log(data.length)
+const findInRow = row => {
+    let countForward = 0;
+    let countBackward = 0;
+    let i = 0;
+    while (i < row.length + 4) {
+        let subRow = row.slice(i, i + 4)
+        let subCol 
+        if (subRow === forwardMatch) {
+            countForward++;
+        } else if (subRow === backwardMatch) {
+            countBackward++;
+        }
+        i++;
+    }
+}
+
+
+// log(lines[0].slice(0, 0 + 4) === 'SAMM')
