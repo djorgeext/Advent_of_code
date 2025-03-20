c6c0581704e92fe33a3ef61d67077593119d5327
djorgeext <djorge.@fi.uba.ar>
2025-03-20 17:09:11 -0300

    last

--- js/day4.js
+++ js/day4.js
@@ -1,7 +1,11 @@
+
 const fs = require('fs');
 const data = fs.readFileSync('input4.txt', 'utf8');
 const log = console.log;
 const lines = data.trim().split('\n');
+
+
+/* // Part 1
 const forwardMatch = "XMAS";
 const backwardMatch = "SAMX";
 const lenMatch = forwardMatch.length;
@@ -22,7 +26,7 @@ const findMatches = lines => {
             if (subStringRow === backwardMatch) {
                 countBackward++;
             }
-            // search in column direction an right diagonal
+            // search in column direction an diagonal upToDown
             if (i < lines.length - lenMatch + 1) {
                 subStringCol = lines[i][j] + lines[i + 1][j] + lines[i + 2][j] + lines[i + 3][j];
                 if (subStringCol === forwardMatch) {
@@ -42,7 +46,7 @@ const findMatches = lines => {
                 }
 
             }
-            // search in left diagonal
+            // search in diagonal downToUp
             if (i >= lenMatch - 1 && j < lines[i].length - lenMatch + 1) {
                 subStringDiagonal = lines[i][j] + lines[i - 1][j + 1] + lines[i - 2][j + 2] + lines[i - 3][j + 3];
                 if (subStringDiagonal === forwardMatch) {
@@ -57,4 +61,30 @@ const findMatches = lines => {
     return countBackward + countForward;
 }
 
-log(findMatches(lines));
+log(findMatches(lines)); */
+
+// Part 2
+const forwardMatch = "MAS";
+const backwardMatch = "SAM";
+const lenMatch = forwardMatch.length;
+
+const xMatches = lines => {
+    let count = 0;
+    let subStringUpToDownDiagonal = '';
+    let subStringDownToUpDiagonal = '';
+
+    for (let i = 0; i < lines.length - lenMatch + 1; i++) {
+        for (let j = 0; j < lines[i].length - lenMatch + 1; j++) {
+            subStringUpToDownDiagonal = lines[i][j] + lines[i + 1][j + 1] + lines[i + 2][j + 2];
+            if (subStringUpToDownDiagonal === forwardMatch || subStringUpToDownDiagonal === backwardMatch) {
+                subStringDownToUpDiagonal = lines[i + 2][j] + lines[i + 1][j + 1] + lines[i][j + 2];
+                if (subStringDownToUpDiagonal === forwardMatch || subStringDownToUpDiagonal === backwardMatch) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+}
+
+log(xMatches(lines));
