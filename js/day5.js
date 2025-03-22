60d7382aaf63672c3d3338c1c23dc86b3c06d582
djorgeext <djorge.ext@fi.uba.ar>
2025-03-22 13:50:20 -0300

    last

---
+++ js/day5.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const data = fs.readFileSync('input5.txt', 'utf8');
+const log = console.log;
+const lines = data.trim().split('\n');
+
+// page order list is a list from lines[0] to lines[1175]
+// each page order list is in the format: 'number | number', and must be converted to an array of two numbers [number, number] 
+const pageOrderList = lines.slice(0, 1176).map(page => page.split('|').map(Number));
+
+// sequence of pages updates from lines[1176] to end
+const pageUpdates = lines.slice(1177).map(page => page.split(',').map(Number));
+
+
+
+
+//log(pageOrderList);
